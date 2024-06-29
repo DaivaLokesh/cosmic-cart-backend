1002a1d2b251c8e3c11f5aa2a45b344985acdc9f
unknown <daivalokash22@gmail.com>
2024-06-29 10:18:22 +0530

    initial commit

---
+++ src/test/java/com/lokesh/onlineshopping/AppTest.java
@@ -0,0 +1,20 @@
+package com.lokesh.onlineshopping;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
